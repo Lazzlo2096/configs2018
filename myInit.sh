c77740c443e48c983d0bdb57ea80288a3ce83131
Lazzlo2096 <lazzlo2096@yandex.ru>
2018-10-02 21:22:22 +0300

    Update myInit.sh

--- myInit.sh
+++ myInit.sh
@@ -1,3 +1,10 @@
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+sudo apt-get update
+yes | sudo apt-get install xorg openbox obconf obmenu #113MB + (xorg:372MB)
+
+===========
+
+
 #UbSvr + lil-mate====
 sudo apt install papirus-icon-theme mate-themes
 
