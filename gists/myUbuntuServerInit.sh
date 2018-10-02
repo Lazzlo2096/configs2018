c1a5a924b6cdac5175f937d5f095d0810d29678b
lazzlo2096 <lazzlo2096@yandex.ru>
2018-10-02 20:01:43 +0000

    8!

---
+++ gists/myUbuntuServerInit.sh
@@ -0,0 +1,87 @@
+{#Если этого не сделал установочник
+#=====настройка времени======
+timedatectl 
+timedatectl set-timezone +3
+timedatectl set-timezone MSK
+
+#=====Язык рус=======
+link1*
+setxkbmap -layout "us,ru(winkeys)" -model "pc105" -option "grp:alt_shift_toggle,grp_led:scroll" &
+}
+
+=====Если с виртуалки==========
+sudo apt install virtualbox-guest-additions-iso
+#Чёт нихуя буфер не работает((
+
+
+
+=====окружение======
+
+	sudo apt-get install xterm
+	sudo apt install firefox
+	sudo apt install leafpad 
+	sudo apt install pcmanfm
+	
+	sudo apt install geany
+	
+
+{#===VIM ang GUI===
+   //42  sudo apt install vim-gtk3
+   //43  vim.gtk3 
+   //44  sudo apt install vim-gtk
+   //45  vim.gt
+   //46  sudo apt remove vim-gtk vim-gtk3
+
+   47  sudo apt install vim-gnome 
+   48  gvi
+   49  sudo apt install neovim
+   50  nvim
+
+   52  sudo apt install neovim-qt
+   53  nvim-qt 
+
+   //58  sudo apt install  vim-athena 
+   //59  vim.athena
+   //60  sudo apt install  vim-nox 
+   //61  vim.nox 
+
+   //62  sudo apt remove  vim-nox vim-athena vim-gtk3 
+   //63  sudo apt remove  vim-nox vim-athena
+   //64  sudo apt install  vim-nox vim-athena
+   66  gvim
+   67  vim --verison
+   //69  sudo apt remove  vim-nox vim-athena
+   70  nvim --version
+   71  echo $VIM
+   72  echo $PATH
+   73  nvim /usr/share/nvim
+   74  nvim-qt 
+   75  nvim
+	
+*Почему-то на xubuntu vim-gtk нормален, а vim-gtk3 уродливые гиганские значки..
+}
+
+===IDEs===
+sudo apt install geany
+
+sudo apt install python3-qt5
+sudo apt install python3-pyqt5 pyqt5-dev-tools qtcreator
+
+> pyuic5 ./ui1.ui -o design.py
+> yqt5-dbg
+
+========
+	##links:
+
+https://gist.github.com/Lazzlo2096/4ed8fedf6d812afefb717ebeaad62f5e
+https://gist.github.com/Lazzlo2096/5a7251fefaf0b44fcc68fd03b61e0cd1
+
+http://openbox.org/wiki/Help:Installing#Dependencies_in_Ubuntu_and_Debian
+
+https://habr.com/post/344298/
+?? tint2 conky lightdm gdebi geany xarchiver
+
+link1: https://help.ubuntu.ru/wiki/openbox
+
+http://openbox.org/wiki/Help:Autostart
+
--- myInit.sh
+++ myInit.sh
@@ -1,39 +1,71 @@
+#========UbSvr + lil-mate======
+#PLEASE install ubuntu-server first, then...
+
+#======Иксы============
+#в версии Ubuntu 18.04.1 LTS bionic - вроде работало ( lsb_release -a )
+#но при новой переустновке (видимо забыл выбрать соурс лист( /etc/apt/source.list ) ) нужно добавить:
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+# & https://askubuntu.com/questions/78613/how-do-i-enable-the-universe-repository-from-the-command-line
 sudo apt-get update
 yes | sudo apt-get install xorg openbox obconf obmenu #113MB + (xorg:372MB)
+	#xorg включает в себя: xserver-xorg, xinit
 
-===========
+#> startx
+#=======Иксы end==========
+
+#====installing and setting utils====
+
+##=pcmanfm=:
+yes | sudo apt install pcmanfm
+sed 's/\(always_show_tabs\)=0/\1=1/' ~/.config/pcmanfm/default/pcmanfm.conf --in-place
+sed 's/\(show_hidden\)=0/\1=1/' ~/.config/pcmanfm/default/pcmanfm.conf --in-place
+
+##=gmrun=:
+#5. вызов приложения по имени по набору, как в i3 awesome?
+#Gmrun (а типа такого, другие есть ещё?)
+#[ https://lugnsk.org/lugnskru/2010/07/prostay-komandnay-stroka-gmrun.html ]
+#[ https://wiki.archlinux.org/index.php/Gmrun ]
+#[ https://www.linuxsecrets.com/archlinux-wiki/wiki.archlinux.org/index.php/Gmrun.html ]
+sudo apt install gmrun
+sudo cp ./inHomeDir/gmrunrc /etc/
+# or manualy in config files which is in /etc/gmrunrc or link in /usr/share/gmrun/gmrun
+#echo "URL_g = x-www-browser 'https://www.google.com/search?q=%s'" >> /etc/gmrunrc
+
+#=gvim=
+
+##=and many others=:
+yes | sudo apt install firefox xterm pluma unzip
+sh "install sublime-text3.sh"
 
 
-#UbSvr + lil-mate====
-sudo apt install papirus-icon-theme mate-themes
-
-# mozhet bez panel'i ?:
-#sudo apt install mate-panel mate-applet-brisk-menu
-# !!! pohodu posle etogo v xterm izmenilsa chvet fona
-# chtoto on u menya so vtorogo zapuska slomalsya(
-
-# utils:
-sudo apt install firefox pcmanfm xterm pluma
-
-
+#==========themes and views==========
+yes | sudo apt install papirus-icon-theme #mate-themes
 cp ./inHomeDir/.gtkrc-2.0 ~/
 # or set it manualy:
 #sudo apt install lxappearance lxappearance-obconf 
+#@depend: (mate-themes) papirus_icons
 
+#=panel=:
+# mozhet bez panel'i ?:
+#yes | sudo apt install mate-panel mate-applet-brisk-menu
+# !!! pohodu posle etogo v xterm izmenilsa chvet fona
+# chtoto on u menya so vtorogo zapuska slomalsya(
 
+#=======other settings======
 
-#settings===
-##=openbox==
+#!set defoult xterm
+
+#=openbox=:
 #=key bindings=
+	#3.горячие клавиши
 # !!!chtoto pohodu sozdalo samo do menya file ~/.config/openbox/rc.xml
 # !!! tochno mate-panel menu perehvatyvaet Win keys...
+#mkdir ~/.config/openbox
 cp ./inHomeDir/.config/openbox/rc.xml ~/.config/openbox/rc.xml
-# or use stevenhoneyman/obkey and set it manualy:
+#@depend from: gmrun, pluma;
+# Or Можно юзнуть эти GUI:
+#https://github.com/nsf/obkey
+#https://github.com/stevenhoneyman/obkey (fork от nsf)
 #cd /opt/; sudo git clone https://github.com/stevenhoneyman/obkey.git;
 #ln /opt/obkey/obkey /usr/bin -s; #or just install it in menu with obconf
 
-##=gmrun==
-#sudo apt install gmrun
-# config files is /etc/gmrunrc #or link in /usr/share/gmrun/gmrun
-#echo "URL_g = firefox 'https://www.google.com/search?q=%s'" >> /etc/gmrunrc
