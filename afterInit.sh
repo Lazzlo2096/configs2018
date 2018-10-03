1599727b770637b06fb0631b2fb68fdf0f0c3859
lazzlo2096 <lazzlo2096@yandex.ru>
2018-10-03 19:16:40 +0000

    8.3.1

---
+++ afterInit.sh
@@ -0,0 +1,10 @@
+ git config --global user.email "lazzlo2096@yandex.ru"
+ git config --global user.name "lazzlo2096"
+
+#==manualy===:
+
+#(& gistfile1_2.txt)#программы по дефолту
+sudo update-alternatives --config x-terminal-emulator
+
+#(& myUbuntuServerInit.sh)
+setxkbmap -layout "us,ru(winkeys)" -model "pc105" -option "grp:alt_shift_toggle,grp_led:scroll" &
