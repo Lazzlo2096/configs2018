3d6ae962d802e8dfa42cb7496d86f2f2d19693a3
lazzlo2096 <lazzlo2096@yandex.ru>
2018-11-04 23:58:58 +0300

    0

--- install eclipse.sh
+++ install eclipse.sh
@@ -1,4 +1,5 @@
+firefox https://www.eclipse.org/downloads/
 sudo apt install openjdk-8-jdk
-#sudo apt install cmake g++ gdb
+sudo apt install cmake g++ gdb ninja-build
 	#sudo apt install cmake --install-recommends --install-suggests 
 	# for ninja
