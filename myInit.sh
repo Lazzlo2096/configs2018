eec410de4a3bb124ddd01c913043cfa41c261cf0
lazzlo2096 <lazzlo2096@yandex.ru>
2018-12-07 18:01:23 +0000

    add background

--- myInit.sh
+++ myInit.sh
@@ -21,6 +21,7 @@ yes | sudo apt-get install xorg openbox obconf obmenu #113MB + (xorg:372MB)
 
 ##=pcmanfm=:
 yes | sudo apt install pcmanfm
+cp ./not\ placed/WallPapers ~/Изображения/ -r
 #sed 's/\(always_show_tabs\)=0/\1=1/' ~/.config/pcmanfm/default/pcmanfm.conf --in-place
 #sed 's/\(show_hidden\)=0/\1=1/' ~/.config/pcmanfm/default/pcmanfm.conf --in-place
 #Видимо конфиг создаёться когда первый раз запускаем его
