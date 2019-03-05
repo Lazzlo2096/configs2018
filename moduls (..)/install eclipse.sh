ce35b0b88dae69af1e6b4753fe24e51143f91f96
lazzlo2096 <lazzlo2096@yandex.ru>
2019-03-06 00:24:11 +0300

    Ср мар  6 00:24:11 MSK 2019

--- moduls (..)/install eclipse.sh
+++ moduls (..)/install eclipse.sh
@@ -1,2 +1,12 @@
 firefox https://www.eclipse.org/downloads/
 sudo apt install openjdk-8-jdk
+#..??
+
+##-----------------
+#sudo apt install eclipse # no c++ new proj template
+#sudo apt install netbeans (gnat?) # stuped ides without c++ projs...
+sudo apt install codelite # so-so
+
+
+#----------
+#vim with giu,,,, or subline with go to declaration
