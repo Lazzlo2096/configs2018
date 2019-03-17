e11b48727f42f369bc3f5dccae7649c19028db7a
lazzlo2096 <lazzlo2096@yandex.ru>
2019-03-17 20:37:15 +0300

    Add bootstrap

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bootstrap
+
+Just do this
+```wget https://raw.githubusercontent.com/Lazzlo2096/configs2018/master/bootstrap.sh | sh -x```
---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#
+#sh -x ./bootstrap.sh 
+
+sudo apt update
+yes | sudo apt install git
+
+mkdir ~/Documents/sandbox
+cd ~/Documents/sandbox
+git clone https://github.com/Lazzlo2096/configs2018.git
+cd ~/Documents/sandbox/configs2018
