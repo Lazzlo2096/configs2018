751eed7c2646c160dba3a2a5930d1ec89333e65f
Lazzlo2096 <lazzlo2096@yandex.ru>
2019-05-25 22:51:38 +0300

    Update bare.sh

--- bare.sh
+++ bare.sh
@@ -6,5 +6,5 @@
 yes | sudo apt install xorg
 #------------
 yes | sudo apt install openbox obmenu obconf #poxoje chto bez OB xorg bespolezen...
-yes | sudo apt install pcmanfm
 yes | sudo apt install vim vim-gtk
+sudo apt install pcmanfm
