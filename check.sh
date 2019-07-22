d6cba305ce366e5908135bb6c3098a28ff1c400a
v.kochnev <you@example.com>
2019-07-22 11:42:11 +0300

    stach

--- check.sh
+++ check.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# g python shell 
+# https://habr.com/ru/post/277679/
+# https://habr.com/ru/post/429892/
+
 repository_name=configs2018
 
 
-files=".bash_aliases"
+files=".bash_aliases" #file1
+path1="."
 
 #проверить что файл существует
 if [ -f ~/$files ]; #или test -f FILENAME
 then
 
+	# проверить в домашней директории
     res=$(diff ./inHomeDir/$files ~/$files)
 
     if [ "" == "$res" ]; then
-        echo YES
+        echo "YES"
     else
         echo "[Warning]: File '$files' in repository '$repository_name/inHomeDir' and '~/' is differs!"
     fi
